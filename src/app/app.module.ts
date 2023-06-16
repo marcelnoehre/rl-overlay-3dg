b72c54426f29420a728a475df44f0b56fbfe3532
marcel-noehre <marcelnoehre@web.de>
2023-06-16 15:50:20 +0200

    basic design for teams

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { ScoreBugComponent } from './components/score-bug/score-bug.component';
+import { TeamsComponent } from './components/teams/teams.component';
+
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ScoreBugComponent
+    ScoreBugComponent,
+    TeamsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
