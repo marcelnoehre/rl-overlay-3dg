85cdd5bfae3ab2fbc966de9a48d923bb64966c54
marcelnoehre <marcelnoehre@web.de>
2023-08-06 15:55:54 +0200

    calc averages for match-overview

--- src/app/components/match-overview/match-overview.component.ts
+++ src/app/components/match-overview/match-overview.component.ts
@@ -1,3 +1,4 @@
+import { KeyedWrite } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Player } from 'src/app/interfaces/player';
 import { Team } from 'src/app/interfaces/team';
@@ -12,11 +13,13 @@ export class MatchOverviewComponent implements OnInit {
   teams: Team[] = [];
   players: Player[][] = [];
   keys: string[] = ['score', 'goals', 'assists', 'saves', 'shots', 'demos', 'touches'];
+  extraKeys: string[] = ['boostConsumption', 'speed'];
+  averages: string[] = [];
 
   constructor(private _data: DataService) { }
 
   ngOnInit(): void {
-    this._data.teams$.subscribe((teams) => {
+  this._data.teams$.subscribe((teams) => {
       this.teams = teams;
     });
     this._data.players$.subscribe((players) => {
@@ -27,6 +30,26 @@ export class MatchOverviewComponent implements OnInit {
         player.speed = parseFloat((player.speed / player.ticks).toFixed(2));
       });
     });
+    for(const key of this.keys) {
+      let sums: number[] = [0, 0];
+      this.players[0].forEach((player) => {
+        sums[0] += Number(player[key]);
+      });
+      this.players[1].forEach((player) => {
+        sums[1] += Number(player[key]);
+      });
+      this.averages.push((sums[0] + sums[1] === 0 ? 50 : 7.5 + (sums[0] / (sums[0] + sums[1])) * (92.5 - 7.5)) + '%');
+    }
+    for(const key of this.extraKeys) {
+      let sums: number[] = [0, 0];
+      this.players[0].forEach((player) => {
+        sums[0] += Number(player[key]);
+      });
+      this.players[1].forEach((player) => {
+        sums[1] += Number(player[key]);
+      });
+      this.averages.push((sums[0] + sums[1] === 0 ? 50 : 7.5 + (sums[0] / (sums[0] + sums[1])) * (92.5 - 7.5)) + '%');
+    }
+    console.log(this.averages);
   }
-
 }
